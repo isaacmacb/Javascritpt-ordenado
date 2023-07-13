74f49bf493dbba97539a74f58a704bcfac100a26
isaacmacb <liberdadebessa@gmail.com>
2023-07-13 00:35:51 -0300

    subindo

---
+++ Explorando a linguagem (avançado)/Map.js
@@ -0,0 +1,7 @@
+// Array de números
+const numbers = [1, 2, 3, 4, 5];
+
+// Usando o método map() para dobrar cada número
+const doubledNumbers = numbers.map((number) => number * 2);
+
+console.log(doubledNumbers);
